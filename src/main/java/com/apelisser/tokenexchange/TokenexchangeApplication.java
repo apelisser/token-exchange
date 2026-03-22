2ef394862cb94890430a180b2ba8ab00cd4a7c52
Abner J Pelisser <abner.pelisser@gmail.com>
2026-03-21 21:03:39 -0300

    add project files

---
+++ src/main/java/com/apelisser/tokenexchange/TokenexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.apelisser.tokenexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenexchangeApplication.class, args);
+	}
+
+}
